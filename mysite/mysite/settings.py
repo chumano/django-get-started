945adfd62dd6e62ae845da673b42784c0302b853
chumano <xuanloc_hoang2000@yahoo.com>
2024-05-01 22:45:01 +0700

    block head for link css

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -179,10 +179,10 @@ USE_TZ = True
 STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static/'),
-    os.path.join(BASE_DIR, 'polls/static/'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'mysite/static/'),
+#     os.path.join(BASE_DIR, 'polls/static/'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
