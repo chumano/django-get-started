76b5a8f036c68355bb6ebb240af14bbcf93b1a61
chumano <xuanloc_hoang2000@yahoo.com>
2024-05-01 10:26:28 +0700

    remove choices url

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
-    path("choices", views.choices, name="choices" ),
+
+    # api
     path("api/", include("polls.api.urls")), 
 ]
